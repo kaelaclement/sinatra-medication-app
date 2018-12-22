cca98bc6094c5915b50894655a435c641241ea90
kaelaclement <stephanie.kaela@gmail.com>
2018-12-22 12:48:56 -0800

    create new user route in user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,13 @@ class UserController < ApplicationController
   get '/signup' do
     erb :'/users/signup'
   end
+
+  post '/users' do
+    user = User.new(params)
+    if !User.find_by(username: params[:username]) && user.save
+      redirect '/medications/index'
+    else
+      redirect '/signup'
+    end
+  end
 end
