a8762ccd7b66b215bb34f47955960fb17365bb30
kaelaclement <stephanie.kaela@gmail.com>
2018-12-24 15:55:32 -0800

    create medication index route and view

--- app/controllers/medication_controller.rb
+++ app/controllers/medication_controller.rb
@@ -1,2 +1,11 @@
 class MedicationController < ApplicationController
+  get '/medications/index' do
+    if logged_in?
+      @user = current_user
+      
+      erb :'/medications/index'
+    else
+      redirect '/login'
+    end
+  end
 end
